c7d96f4ea162ce3037bdba802c09270934b9edc7
BMARLE <bertrand.marle.atos@gmail.com>
2018-08-03 17:48:18 +0200

    Signed-off-by: BMARLE <bertrand.marle.atos@gmail.com>

---
+++ katalon_git_project/Libs/TempTestSuite1533311194508.groovy
@@ -0,0 +1,35 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import groovy.lang.MissingPropertyException
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+
+suiteProperties.put('id', 'Test Suites/Suite001')
+
+suiteProperties.put('name', 'Suite001')
+
+suiteProperties.put('description', '')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\A124942\\Desktop\\katalon_git\\katalon_git_project\\Reports\\Suite001\\20180803_174634\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/Suite001', suiteProperties, [new TestCaseBinding('Test Cases/TC_001', 'Test Cases/TC_001',  null)])
---
+++ katalon_git_project/Test Suites/Suite001.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Suite001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-08-03T17:46:34</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c0b4f3d4-ec20-4300-9b4d-5fcd3127a9c8</testSuiteGuid>
+   <testCaseLink>
+      <guid>7e138771-07c3-45cc-9dcc-ea767c0ac4d7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
